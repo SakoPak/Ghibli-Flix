cecc2adf7dd7dfd258a2078bc8709602cc57a9f4
SakoPak <sako.taya@gmail.com>
2022-05-27 03:02:02 -0700

    Add Api config link

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://afternoon-earth-27940.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
